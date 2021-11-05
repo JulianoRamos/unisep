4df45503acfcaad59c74aa7d9bab6042c33ac515
Juliano Ramos <490meister@gmail.com>
2021-11-04 23:11:22 -0300

    feat: deploy

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,70 @@
 import React from 'react';
 import { Flex, Stack, Button } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import Input from '../../components/shared/Input';
+import { useAuth } from '../../hooks/auth';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
 
 const SignIn: React.FC = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const { errors, isSubmitting } = formState;
+
+  const { signIn } = useAuth();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async values => {
+    await signIn(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
+        as="form"
         width="100%"
         maxW="360"
         bg="gray.800"
         p={8}
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <input />
-          <input />
+          <Input
+            type="email"
+            label="E-mail"
+            error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            type="password"
+            label="Senha"
+            error={errors.password}
+            {...register('password')}
+          />
         </Stack>
 
-        <Button type="submit" mt={6} colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt={6}
+          colorScheme="pink"
+          size="lg"
+          isLoading={isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
